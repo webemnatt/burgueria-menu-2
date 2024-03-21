33c49be044eb104b0ab6ba5c5f074187026f6bc2
webemnatt <webemnatt@gmail.com>
2024-03-21 18:35:52 -0300

    feat: showed the cartItems on the modal

--- app/footer/cartModal.tsx
+++ app/footer/cartModal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { cartItems } from '../main/menu';
 
 interface CartModalProps {
   isOpen: boolean;
@@ -7,25 +8,39 @@ interface CartModalProps {
 }
 
 const CartModal: React.FC<CartModalProps> = ({ isOpen, whiteModal, openCloseModal }) => {
-  
+  let total = 0
   return (
     <div
       id="cart-modal"
       className={`bg-black/60 w-full h-full fixed top-0 left-0 z- items-center justify-center ${
         isOpen ? 'flex' : 'hidden'
       }`}
-      onClick={whiteModal} // Fecha o modal quando clicado fora dele
+      onClick={whiteModal}
     >
       <div className="bg-white p-5 rounded-md min-w-[90%] md:min-w-[600px]">
         <h2 className="text-center font-bold text-2xl mb-2">Meu Carrinho</h2>
-        <div id="cart-item" className="flex justify-between mb-2 flex-col"></div>
+        <div id="cart-item" className="flex justify-between mb-2 flex-col">
+        <ul> 
+          {cartItems.map((item, index) => (
+            <li key={index} className="flex justify-between mb-2">
+              <span>{item.name}</span>
+              <span>R$ {item.price}</span>
+              <span>Quantidade ({item.quantity})</span>
+            </li>
+          ))}
+        </ul>
+        </div>
         <div className=" flex justify-end ">
-          <p className="flex justify-between bg-black text-white p-1.5 rounded-md">Total: &nbsp;<span>R$ &nbsp;<span id="cart-total">0.00</span></span></p>
+          {cartItems.map(item => {
+            let value = item.price * item.quantity
+            total += value
+            return null
+          })}
+          <p className="flex justify-between bg-black text-white p-1.5 rounded-md">Total: &nbsp;<span>R$ &nbsp;<span id="cart-total">{(total).toFixed(2)}</span></span></p>
         </div>
         <p className="font-bold mt-4">Endereço de entrega:</p>
         <input type="text" placeholder="Digite seu endereço completo" id="address" className="w-full border-2 p-1 rounded my-1" />
         <p id="address-warn" className="text-red-500 hidden">Digite seu endereço completo!</p>
-
         <div className="flex items-center justify-between mt-5 w-full">
           <button id="close-cart-modal-btn" type="button" onClick={openCloseModal}>Fechar</button>
           <button id="checkout-btn" className="bg-green-500 text-white px-4 py-1 rounded">Finalizar pedido</button>
--- app/main/menu/index.tsx
+++ app/main/menu/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Items from "./items";
-import { useCart } from './vielmodel'
+import { useCart } from './viewmodel'
 
 interface MenuItem {
   id: string;
@@ -14,40 +14,34 @@ interface MenuProps {
   burgersList: MenuItem[];
   drinksList: MenuItem[];
 }
+
+let cartItems:any[] = []
+
 // componente que exibe o menu de burgers e bebidas
 const Menu:React.FC<MenuProps> = ({burgersList,drinksList}) => {
   const { addToCart, getCartItems } = useCart()
-  const items = getCartItems()
+  cartItems = getCartItems()
 
   const handleAddToCart = (name: string, price: number) => {
     addToCart(name, price)
   }
-  
-  const handleGetCartItems = () => {
-    console.log("clicou no botão do menu",items)
-  }
+
+  let total = 0;
 
   return (
     <section id='menu' className="p-10">
       <h2 className="text-center text-4xl p-6">Conheça nosso menu</h2>
       <Items id='burgers' itemList={burgersList} gridRows={4} onAddToCart={handleAddToCart}/>
-      <button onClick={handleGetCartItems}>
-        Obter Itens do Carrinho
-      </button>
-      <ul> 
-          {items.map((item, index) => (
-            <li key={index} className="flex justify-between mb-2">
-              <span>{item.name}</span>
-              <span>R$ {item.price}</span>
-              <span>Quantidade ({item.quantity})</span>
-            </li>
-          ))}
-        </ul>
+      {cartItems.map(item => {
+        total += item.quantity
+        return null
+      })}
+      <p>Total de quantidade: {total}</p>
       <h2 className="text-center text-4xl p-6">Bebidas</h2>
       <Items id='drinks' itemList={drinksList} gridRows={2} onAddToCart={handleAddToCart}/>
-
     </section>
   );
 }
 
+export { cartItems }; // Exportando a variável items
 export default Menu
